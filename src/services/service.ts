38ea1d9577b99ece97856d1cb99a8e4a83dbe510
ekozmdev <eiki.ozama.mlsm@outlook.com>
2023-12-06 22:57:39 +0900

    fix ちょっと修正

--- src/services/service.ts
+++ src/services/service.ts
@@ -3,13 +3,13 @@ import { AxiosPromise, AxiosRequestConfig } from 'axios'
 import { MsGraphMeResponse } from '../types/MsGraphMeResponse'
 
 export const getMsGraphMe = (
-  accesstoken: string
+  accessToken: string
 ): AxiosPromise<MsGraphMeResponse> => {
   const config: AxiosRequestConfig = {
     method: 'GET',
     url: 'https://graph.microsoft.com/v1.0/me',
     headers: {
-      Authorization: `Bearer ${accesstoken}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   }
   return axios(config)
